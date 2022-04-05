dae686feeba3dfb5e2ee49f91bc7df3c904c13d3
Fenosoa Andrianirina <fensooandrian@gmail.com>
2022-04-05 15:50:21 +0200

    Creation of a simple jQuery accordion

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Simple jQuery accordion </title>
+	<meta charset="utf-8">
+	<link rel="icon" href="component/image/icon.jpg">
+	<link rel="stylesheet" type="text/css" href="component/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="component/css/app.css">
+    <link rel="stylesheet" type="text/css" href="component/font/css/all.min.css">
+   
+</head>
+<body>
+    <div class="container accordion_area pt-5"> 
+      
+            <div class="col-md-12 accordion_box "> 
+                <h3 class="acc_trigger ">Accordion Title 1
+                    <i class="fas fa-plus" aria-hidden="true"></i>
+                </h3>
+                <div class="acc_container">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                        Aliquid eos asperiores molestiae pariatur aliquam. 
+                        Quibusdam pariatur magni aliquid iure delectus deserunt, 
+                        nemo similique laborum nulla facilis. Quis cupiditate architecto officia.
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                        Aliquid eos asperiores molestiae pariatur aliquam. 
+                        Quibusdam pariatur magni aliquid iure delectus deserunt, 
+                        nemo similique laborum nulla facilis. Quis cupiditate architecto officia.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-12 mx-auto accordion_box"> 
+                <h3 class="acc_trigger ">Accordion Title 2
+                    <i class="fas fa-plus" aria-hidden="true"></i>
+                </h3>
+                <div class="acc_container">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                        Aliquid eos asperiores molestiae pariatur aliquam. 
+                        Quibusdam pariatur magni aliquid iure delectus deserunt, 
+                        nemo similique laborum nulla facilis. Quis cupiditate architecto officia.
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                        Aliquid eos asperiores molestiae pariatur aliquam. 
+                        Quibusdam pariatur magni aliquid iure delectus deserunt, 
+                        nemo similique laborum nulla facilis. Quis cupiditate architecto officia.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-12 mx-auto accordion_box"> 
+                <h3 class="acc_trigger ">Accordion Title 3
+                    <i class="fas fa-plus" aria-hidden="true"></i>
+                </h3>
+                <div class="acc_container">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                        Aliquid eos asperiores molestiae pariatur aliquam. 
+                        Quibusdam pariatur magni aliquid iure delectus deserunt, 
+                        nemo similique laborum nulla facilis. Quis cupiditate architecto officia.
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                        Aliquid eos asperiores molestiae pariatur aliquam. 
+                        Quibusdam pariatur magni aliquid iure delectus deserunt, 
+                        nemo similique laborum nulla facilis. Quis cupiditate architecto officia.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-12 mx-auto accordion_box"> 
+                <h3 class="acc_trigger ">Accordion Title 4
+                    <i class="fas fa-plus" aria-hidden="true"></i>
+                </h3>
+                <div class="acc_container">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                        Aliquid eos asperiores molestiae pariatur aliquam. 
+                        Quibusdam pariatur magni aliquid iure delectus deserunt, 
+                        nemo similique laborum nulla facilis. Quis cupiditate architecto officia.
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                        Aliquid eos asperiores molestiae pariatur aliquam. 
+                        Quibusdam pariatur magni aliquid iure delectus deserunt, 
+                        nemo similique laborum nulla facilis. Quis cupiditate architecto officia.
+                    </p>
+                </div>
+            </div>
+    </div>
+
+   
+ 
+<script type="text/javascript" src="component/js/jquery-2.2.3.min.js"></script>
+<script src="component/js/anim.js" ></script>
+</body>
+</html>
